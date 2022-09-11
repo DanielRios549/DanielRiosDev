a074e71fed7854573604adba9ed398db0619a2a3
Daniel Rios <danielrios.flamengo@gmail.com>
2022-09-11 17:53:39 -0300

    Add ENV Var Types

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,1 +1,14 @@
 declare module '*.scss'
+
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            readonly URL: string
+            readonly PREFIX: string
+            readonly API_URL: string
+            readonly API_KEY: string
+        }
+    }
+}
+
+export {}
