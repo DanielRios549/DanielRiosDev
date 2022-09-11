58d86ea2081090c36aee2e9f33454ef00e28d57e
Daniel Rios <danielrios.flamengo@gmail.com>
2022-09-11 17:53:07 -0300

    Get Page Title Dynamically

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
+import React from 'react'
+import Head from 'next/head'
 import { AppProps } from 'next/app'
 import '../styles/app.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <React.StrictMode>
+            <Head>
+                <link rel="icon" href="/favicon.png" />
+            </Head>
+            <Component {...pageProps} />
+        </React.StrictMode>
+    )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,32 @@
 import Head from 'next/head'
+import type { GetStaticProps } from 'next'
+
 import style from 'styles/pages/index.module.scss'
 
-export default function Home() {
+type Props = {
+    title: string
+}
+
+export default function Home({ title }: Props) {
     return (
         <>
             <Head>
-                <title>Daniel Rios</title>
-                <link rel="icon" href="/favicon.png" />
+                <title>{title}</title>
             </Head>
             <main id={style.main}>
                 <h1>Coming soon</h1>
             </main>
         </>
     )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const url = process.env.URL
+
+    const get = await fetch(`${url}/api/info/title`)
+    const title = await get.json()
+
+    return {
+        props: { title }
+    }
 }
