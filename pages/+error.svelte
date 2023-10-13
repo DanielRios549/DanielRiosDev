aee60040a130312921b9b91acb51a9cb2710db32
Daniel Rios <danielrios.oficial@gmail.com>
2023-10-13 20:37:29 -0300

    Add Error Page

---
+++ pages/+error.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { page } from '$app/stores'
+</script>
+
+<section>
+    <header>
+        <h1>{$page.status}: {$page.error?.message}</h1>
+    </header>
+</section>
+
+<style lang="scss">
+    section {
+        @extend %center;
+
+        color: var(--text);
+        font-size: 2rem;
+        height: 100%;
+    }
+</style>
