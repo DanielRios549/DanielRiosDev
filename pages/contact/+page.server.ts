c6bfe08b53b62e100b4adc8d33d320c9e7edc2c7
Daniel Rios <danielrios.oficial@gmail.com>
2023-10-23 11:36:16 -0300

    Wrap Mail Sections in Promises

--- pages/contact/+page.server.ts
+++ pages/contact/+page.server.ts
@@ -1,5 +1,4 @@
 import { createTransport } from 'nodemailer'
-import { fail } from '@sveltejs/kit'
 import { env } from '$env/dynamic/private'
 
 export const actions = {
@@ -16,55 +15,72 @@ export const actions = {
             }
         })
 
-        // To the one who contact me
-        const confirmation = {
-            from: `Daniel Rios Contact <${env.MAIL}>`,
-            to: data.email,
-            subject: 'Form Contact to Daniel Rios',
-            replyTo: env.TRANSPORT_USER,
-            html: /* html */ `
-                <p>Thank you for reaching out to me. I will respond as soon as possible.</p>
-                <p>Feel free to reach out me again at: ${env.TRANSPORT_USER}</p>
-                <p>Daniel Rios.</p><br />
-                <p>This is the message you sent me:</p>
-                <p>-----------------------------</p>
-                <p>${data.message}</p>
-            `
-        }
-
-        transporter.sendMail(confirmation, (err) => {
-            if (err) {
-                return fail(500, {
-                    error: err.cause,
-                    message: err.message
-                })
-            }
+        // Verify if configuration is correct
+        await new Promise((resolve, reject) => {
+            transporter.verify((err, success) => {
+                if (err) {
+                    console.log(err)
+                    reject(err)
+                }
+                else {
+                    resolve(success)
+                }
+            })
         })
 
-        // To myself
-        const contact = {
-            from: `Form Contact <${data.email}>`,
-            to: env.MAIL,
-            subject: data.subject || `Form Contact from ${data.email}`,
-            html: /* html */ `
-                <p>Name: ${data.name}</p>
-                <p>Email: ${data.email}</p>
-                <p>-----------------------------</p>
-                <p>${data.message}</p>
-            `
-        }
-
-        transporter.sendMail(contact, (err) => {
-            if (err) {
-                return fail(500, {
-                    error: err.cause,
-                    message: err.message
-                })
+        // Send email to the one who contact me
+        await new Promise((resolve, reject) => {
+            const confirmation = {
+                from: `Daniel Rios Contact <${env.MAIL}>`,
+                to: data.email,
+                subject: 'Form Contact to Daniel Rios',
+                replyTo: env.TRANSPORT_USER,
+                html: /* html */ `
+                    <p>Thank you for reaching out to me. I will respond as soon as possible.</p>
+                    <p>Feel free to reach out me again at: ${env.TRANSPORT_USER}</p>
+                    <p>Daniel Rios.</p><br />
+                    <p>This is the message you sent me:</p>
+                    <p>-----------------------------</p>
+                    <p>${data.message}</p>
+                `
             }
+            transporter.sendMail(confirmation, (err, info) => {
+                if (err) {
+                    console.log(err)
+                    reject(err)
+                }
+                else {
+                    resolve(info)
+                }
+            })
+        })
+
+        // Send email to myself
+        await new Promise((resolve, reject) => {
+            const contact = {
+                from: `Form Contact <${data.email}>`,
+                to: env.MAIL,
+                subject: data.subject || `Form Contact from ${data.email}`,
+                html: /* html */ `
+                    <p>Name: ${data.name}</p>
+                    <p>Email: ${data.email}</p>
+                    <p>-----------------------------</p>
+                    <p>${data.message}</p>
+                `
+            }
+            transporter.sendMail(contact, (err, info) => {
+                if (err) {
+                    console.log(err)
+                    reject(err)
+                }
+                else {
+                    resolve(info)
+                }
+            })
         })
 
         return {
-            message: 'Email Sent '
+            message: 'Email Sent'
         }
     }
 }
