2bc4a8619b935429c2efefa22c5cdb839df14c07
Daniel Rios <danielrios.oficial@gmail.com>
2023-11-03 15:03:27 -0300

    Add Missing Icons

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,20 @@ import svg from '@poppanator/sveltekit-svg'
 import { resolve } from 'path'
 
 const config = defineConfig({
-    plugins: [svg(), sveltekit()],
+    plugins: [
+        svg({
+            svgoOptions: {
+                plugins: [
+                    'preset-default',
+                    'prefixIds'
+                ]
+            }
+        }),
+        sveltekit()
+    ],
+    css: {
+        devSourcemap: true
+    },
     server: {
         port: 3000,
         strictPort: true
